42eb285a9b8d9274d5ce2efe774869d22f42413e
王鑫 <jiugewx@qq.com>
2017-03-13 16:23:58 +0800

    开始启用mysql

--- fang-tian-xia/main.js
+++ fang-tian-xia/main.js
@@ -1,16 +1,15 @@
-/**
- * Created by xinye on 2017/3/12.
- */
 var http = require('http');
 var fs = require('fs');
 var cheerio = require('cheerio');
 const charset = require('superagent-charset');
 const request = require('superagent');
 charset(request);
+var moment = require('moment');
+var utils = require("../utils/base.js");
 var host = "http://newhouse.cs.fang.com";
-// var url = "http://newhouse.cs.fang.com/house/s/b810-c3110%2C144-c9y/";
-var Cookie = "global_cookie=rllvehz9y2q14m1ryta50owtk1oj06dg2kg; searchLabelN=3_1489304518_7921%5B%3A%7C%40%7C%3A%5Da2174d3aff45f973894f38c179c10916; searchConN=3_1489304518_8494%5B%3A%7C%40%7C%3A%5D0ccd291a349d353ce7b923f9f0d64dce; new_search_uid=17ff89ad7f21138ce6a4cf326794b5f3; newhouse_chat_guid=E961597A-4220-E796-D656-C901F55EF7FE; jiatxShopWindow=1; Captcha=58384A542F685176473068676663774C4C6B6C6748314F784279636C4F36715161596C747930735370464D4D4B4C6A345179794A7A666C4A4D4D344D6F343431367265692F706844412F6B3D; vh_newhouse=3_1489304706_13282%5B%3A%7C%40%7C%3A%5D47891578c8babbd19391aec38709590c; newhouse_user_guid=109E4B2A-AEEB-1EC1-B055-99572AE77737; __utmt_t0=1; __utmt_t1=1; __utmt_t2=1; sf_source=; s=; city=cs; indexAdvLunbo=; __utmt_t3=1; __utmt_t4=1; __utma=147393320.179223313.1489304500.1489304500.1489309684.2; __utmb=147393320.31.10.1489309684; __utmc=147393320; __utmz=147393320.1489309684.2.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; unique_cookie=U_rllvehz9y2q14m1ryta50owtk1oj06dg2kg*25";
 
+// var url = "http://newhouse.cs.fang.com/house/s/b810-c3110%2C144-c9y/";
+var Cookie = "showAdquanguo=1; global_cookie=v187c7ws8owbnammasza0lwmk15j07gso6u; indexAdvLunbo=lb_ad5%2C0; __utmt_t0=1; __utmt_t1=1; __utmt_t2=1; sf_source=; s=; showAdcs=1; city=cs; __utmt_t3=1; __utmt_t4=1; new_search_uid=53ed90827b4a0259bddf88df77abd70c; __utma=147393320.1395782459.1489370594.1489370594.1489370594.1; __utmb=147393320.44.10.1489370594; __utmc=147393320; __utmz=147393320.1489370594.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; newhouse_chat_guid=102E4B2A-2A13-CEB4-E1B6-31B4754C73DA; jiatxShopWindow=1; coderes=xJjiPHvGEyHGnUeg; Captcha=4E546151324771764C634D316432744B576A39565459704A48376A4F4655527A675557627331466E763076643642457438662F7936414D52434570385278667A32422B42663654335550383D; newhouse_user_guid=94B33913-FAB0-9695-0368-BD8260D4F5D9; unique_cookie=U_v187c7ws8owbnammasza0lwmk15j07gso6u*9";
 
 function requestUrl(url, id) {
     return new Promise(function (resolve, reject) {
@@ -19,7 +18,7 @@ function requestUrl(url, id) {
             .set('Cookie', Cookie)
             .charset('gbk')
             .end(function (err, res) {
-                if (err) {
+                if ( err ) {
                     console.log(err);
                     reject();
                 }
@@ -42,7 +41,7 @@ function getSummary(url) {
             .set('Cookie', Cookie)
             .charset('gbk')
             .end(function (err, res) {
-                if (err) {
+                if ( err ) {
                     console.log(err);
                 }
                 var html = res.text;
@@ -83,30 +82,31 @@ function getSummary(url) {
 
 // 获取其他详情
 function getAsyncSingleDetail(url, id) {
-    return requestUrl(url, id)
-        .then(function (data) {
-            return new Promise(function (res, rej) {
-                var id = data.id;
-                var $ = cheerio.load(data.content, {decodeEntities: false});
-                var items = $(".right_box_zzlxnrl.hidden .f14");
-                var addressHtml = $(items[items.length - 1]);
-                var districtHtml = addressHtml.find("span.fl").text();
-                var district = districtHtml.replace(/&nbsp;/g, " ").replace(/\[|\]/g, "").split("：")[1];
-                var address = addressHtml.find(">a").text();
-                var newUrl = data.url.split("fang.com/")[0] + "fang.com/dianping/";
-                allObject[id]["address"] = district + address;
-                allObject[id]["district"] = district;
-                var resData = {
-                    id: id,
-                    url: newUrl
-                };
-
-                res(resData)
-            })
-        })
-        .then(function (data) {
-            return requestUrl(data.url, data.id)
-        })
+    // return requestUrl(url, id)
+    // .then(function (data) {
+    //     return new Promise(function (res, rej) {
+    //         var id = data.id;
+    //         var $ = cheerio.load(data.content, {decodeEntities: false});
+    //         var items = $(".right_box_zzlxnrl.hidden .f14");
+    //         var addressHtml = $(items[items.length - 1]);
+    //         var districtHtml = addressHtml.find("span.fl").text();
+    //         var district = districtHtml.replace(/&nbsp;/g, " ").replace(/\[|\]/g, "").split("：")[1];
+    //         var address = addressHtml.find(">a").text();
+    //         var newUrl = data.url.split("fang.com/")[0] + "fang.com/dianping/";
+    //         allObject[id]["address"] = district + address;
+    //         allObject[id]["district"] = district;
+    //         var resData = {
+    //             id: id,
+    //             url: newUrl
+    //         };
+    //
+    //         res(resData)
+    //     })
+    // })
+    // .then(function (data) {
+    var newUrl = url.split("fang.com/")[0] + "fang.com/dianping/";
+    return requestUrl(newUrl, id)
+    // })
         .then(function (data) {
             return new Promise(function (res, rej) {
                 var id = data.id;
@@ -117,7 +117,7 @@ function getAsyncSingleDetail(url, id) {
 
                 allObject[id]["count"] = count;
                 allObject[id]["score"] = Number(comment_font + "." + comment_last);
-                // console.log(allObject[id]);
+                console.log(allObject[id]);
                 res(allObject[id])
             })
         });
@@ -125,8 +125,8 @@ function getAsyncSingleDetail(url, id) {
 
 function getAllDetail(data) {
     allObject = data;
-    for (var name in data) {
-        AsyncArray.push(getAsyncSingleDetail(data[name].detail, name))
+    for (var id in data) {
+        AsyncArray.push(getAsyncSingleDetail(data[id].detail, id))
     }
     return Promise.all(AsyncArray); // resolve(一个数组)
 }
@@ -134,13 +134,15 @@ function getAllDetail(data) {
 var allObject = {};
 var AsyncArray = [];
 var page = [
-    "/house/s/b810-b91-c3110%2C144-c9y/",
-    "/house/s/b810-b92-c3110%2C144-c9y/",
-    "/house/s/b810-b93-c3110%2C144-c9y/",
-    "/house/s/b810-b94-c3110%2C144-c9y/",
-    "/house/s/b810-b95-c3110%2C144-c9y/",
-    "/house/s/b810-b96-c3110%2C144-c9y/",
-    "/house/s/b810-b97-c3110%2C144-c9y/",
+    "/house/s/b810-b91-c390%2C144-c9y/",
+    "/house/s/b810-b92-c390%2C144-c9y/",
+    "/house/s/b810-b93-c390%2C144-c9y/",
+    "/house/s/b810-b94-c390%2C144-c9y/",
+    "/house/s/b810-b95-c390%2C144-c9y/",
+    "/house/s/b810-b96-c390%2C144-c9y/",
+    "/house/s/b810-b97-c390%2C144-c9y/",
+    "/house/s/b810-b98-c390%2C144-c9y/",
+    "/house/s/b810-b99-c390%2C144-c9y/"
 ];
 var PageArray = [];
 
@@ -150,39 +152,43 @@ function getPage(url) {
 
 function mergeArray2Object(array) {
     var data = {};
-    for (var i = 0; i < array.length; i++) {
+    for (var i = 0; i < array.length; i ++) {
         data = Object.assign(data, array[i]);
     }
     return data;
 }
-for (var k = 0; k < page.length; k++) {
-    PageArray.push(getPage(host + page[k]))
-}
-
-Promise.all(PageArray).then(function (result) {
-    return new Promise(function (res, rej) {
-        var data = mergeArray2Object(result);
-        res(data);
-    })
-}).then(getAllDetail).then(
-    function (data) {
-        data.sort(function (a, b) {
-            return a.score < b.score
-        });
-        var json = JSON.stringify(data);
-        savedContent("sortByScore", json);
-    }
-).catch(function (error) {
-    console.log("出错了：",error)
-});
-
 
 //该函数的作用：在本地存储所爬取的json
 function savedContent(news_title, content) {
-    var time = new Date().getTime();
+    var time = moment().format("YYYYMMDDhhmmss");
     fs.appendFile('./data/' + news_title + time + '.JSON', content, 'utf-8', function (err) {
-        if (err) {
+        if ( err ) {
             console.log(err);
         }
     });
 }
+
+
+for (var k = 0; k < page.length; k ++) {
+    PageArray.push(getPage(host + page[k]))
+}
+
+//
+Promise.all(PageArray)
+    .then(function (result) {
+        return new Promise(function (res, rej) {
+            var data = mergeArray2Object(result);
+            res(data);
+        })
+    })
+    .then(getAllDetail)
+    .then(function (data) {
+        data.sort(function (a, b) {
+            return a.score < b.score
+        });
+        var json = JSON.stringify(data);
+        savedContent("data", json);
+    })
+    .catch(function (error) {
+        console.log("出错了：", error)
+    });
--- sequelize.js
+++ sequelize.js
@@ -1,3 +1,43 @@
-/**
- * Created by wangxin on 2017/3/13.
- */
+var Sequelize = require('sequelize');
+var moment = require('moment');
+var co = require('co');
+var sequelize = new Sequelize('job', 'root', '');
+
+
+// 定义单张表
+var User = sequelize.define(
+    'test',
+    {
+        'emp_id': {
+            'type': Sequelize.CHAR(10), // 字段类型
+            'allowNull': false,         // 是否允许为NULL
+            'unique': true              // 字段是否UNIQUE
+        },
+        'nick': {
+            'type': Sequelize.CHAR(10),
+            'allowNull': false
+        },
+        'department': {
+            'type': Sequelize.STRING(64),
+            'allowNull': true
+        }
+    }
+);
+
+// // 插入数据
+User.create({
+    emp_id: '232',
+    nick: "mik33e",
+    department: "1q2223w"
+})
+    .then(function (jane) {
+        console.log(jane.get());
+    })
+    .then(function () {
+        return User.findAll()
+    })
+    .then(function (result) {
+        for (var i = 0, usr; usr = result[i ++];) {
+            console.log('name=' + usr.nick + ', id=' + usr.emp_id + ', part=' + usr.department);
+        }
+    });
